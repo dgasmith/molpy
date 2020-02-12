244ac11ddfb3572be1056418c958be19f5a1e5fb
Daniel G. A. Smith <dgasmith@vt.edu>
2020-02-12 13:31:32 +0100

    Util: Adds test for distance

---
+++ molpy/tests/test_util.py
@@ -0,0 +1,7 @@
+
+import molpy
+
+def test_distance():
+
+    assert molpy.util.distance([0, 1], [0, 0]) == 1
+    
--- molpy/util.py
+++ molpy/util.py
@@ -3,4 +3,6 @@ import numpy as np
 
 def distance(point1, point2):
     
+    point1 = np.asarray(point1)    
+    point2 = np.asarray(point2)
     return np.linalg.norm(point1 - point2)    

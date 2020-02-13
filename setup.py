a9bfe80aca925f0f39204f1287db83170dd8a881
Daniel G. A. Smith <dgasmith@vt.edu>
2020-02-13 14:58:59 +0100

    pypi release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except:
 
 setup(
     # Self-descriptive entries which should always be present
-    name="molpy",
+    name="molpy-dgas",
     author="Daniel Smith",
     author_email="dgasmith@vt.edu",
     description=short_description[0],
